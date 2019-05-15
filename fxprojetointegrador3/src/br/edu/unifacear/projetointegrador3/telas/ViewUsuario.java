d10213bbe01728d3d607641e861728919e8954a9
CLEVERSON AVELINO Ferreira <cleversonavelino@gmail.com>
2019-05-15 20:14:58 -0300

    code changes

--- fxprojetointegrador3/src/br/edu/unifacear/projetointegrador3/telas/ViewUsuario.java
+++ fxprojetointegrador3/src/br/edu/unifacear/projetointegrador3/telas/ViewUsuario.java
@@ -4,17 +4,21 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import br.edu.unifacear.projetointegrador3.dao.Dao;
 import br.edu.unifacear.projetointegrador3.dao.GenericDao;
 import br.edu.unifacear.projetointegrador3.entidade.Usuario;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -24,6 +28,26 @@ public class ViewUsuario extends Application implements Initializable {
 	@FXML TableColumn<Usuario,Integer> id;
 	@FXML TableColumn<Usuario,String> nome;
 	@FXML TableColumn<Usuario,String> login;
+	
+	@FXML Button btnSalvar;
+	@FXML TextField txtId;
+	@FXML TextField txtNome;
+	@FXML TextField txtLogin;
+	@FXML TextField txtSenha;
+	
+	@FXML public void salvar(ActionEvent event) {
+		Usuario usuario = new Usuario();
+		usuario.setNome(txtNome.getText());
+		usuario.setLogin(txtLogin.getText());
+		usuario.setSenha(txtSenha.getText());
+		
+		Dao<Usuario> usuarioDao = new GenericDao<Usuario>();
+		usuarioDao.inserir(usuario);
+		
+		List<Usuario> usuarios = new GenericDao<Usuario>().listarTodos(Usuario.class);
+		ObservableList<Usuario> data = FXCollections.observableArrayList(usuarios);
+		tabelaUsuario.setItems(data);
+	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
